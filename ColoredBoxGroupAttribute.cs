a52690d8fd32f8f82f4e2118864c9c29514d5407
Brandon Ingram <57206908+Kurloc@users.noreply.github.com>
2021-04-25 18:13:43 -0700

    Create ColoredBoxGroupAttribute.cs

---
+++ ColoredBoxGroupAttribute.cs
@@ -0,0 +1,29 @@
+using System;
+using Sirenix.OdinInspector;
+
+
+#pragma warning disable
+[AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = true)]
+[global::System.Diagnostics.Conditional("UNITY_EDITOR")]
+public class ColoredBoxGroupAttribute : BoxGroupAttribute
+{
+    public float R, G, B, A;
+    public bool BoldLabel;
+
+
+    public ColoredBoxGroupAttribute(
+        string @group,
+        float r, float g, float b, float a,
+        bool showLabel = true,
+        bool centerLabel = false,
+        bool boldLabel = false,
+        float order = 0) : base(@group, showLabel, centerLabel, order)
+    {
+        R = r;
+        G = g;
+        B = b;
+        A = a;
+
+        BoldLabel = boldLabel;
+    }
+}
