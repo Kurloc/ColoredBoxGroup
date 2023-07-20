f6cd92cad95829d8dfb405962999f82e600abc41
Brandon Ingram <57206908+Kurloc@users.noreply.github.com>
2023-07-20 18:58:52 -0500

    Update ColoredBoxGroupAttribute.cs

--- ColoredBoxGroupAttribute.cs
+++ ColoredBoxGroupAttribute.cs
@@ -1,60 +1,61 @@
 using System;
-using Sirenix.OdinInspector;
 
-
-// #pragma warning disable
-[AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = true)]
-[System.Diagnostics.Conditional("UNITY_EDITOR")]
-public class ColoredBoxGroupAttribute : BoxGroupAttribute
+namespace Sirenix.OdinInspector.ColoredBoxGroup
 {
-    public readonly float R, G, B, A;
-    public readonly bool  BoldLabel;
-
-    public int MarginTop    { get; set; }
-    public int MarginBottom { get; set; }
-
-    public ColoredBoxGroupAttribute(
-        string @group,
-        float  r = 0,
-        float  g = 0,
-        float  b = 0,
-        float  a = 0,
-        int    marginTop   = 0,
-        bool   showLabel   = true,
-        bool   centerLabel = false,
-        bool   boldLabel   = false,
-        float  order       = 0
-    ) : base(@group, showLabel, centerLabel, order)
+    // #pragma warning disable
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = true)]
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public class ColoredBoxGroupAttribute : BoxGroupAttribute
     {
-        R         = r;
-        G         = g;
-        B         = b;
-        A         = a;
-        MarginTop = marginTop;
+        public readonly float R, G, B, A;
+        public readonly bool  BoldLabel;
 
-        BoldLabel = boldLabel;
-    }
+        public int MarginTop    { get; set; }
+        public int MarginBottom { get; set; }
 
-    public ColoredBoxGroupAttribute(
-        string @group,
-        string label,
-        float  r = 0,
-        float  g = 0,
-        float  b = 0,
-        float  a = 0,
-        int    marginTop   = 0,
-        bool   showLabel   = true,
-        bool   centerLabel = false,
-        bool   boldLabel   = false,
-        float  order       = 0
-    ) : base(@group, showLabel, centerLabel, order)
-    {
-        R         = r;
-        G         = g;
-        B         = b;
-        A         = a;
-        MarginTop = marginTop;
-        LabelText = label;
-        BoldLabel = boldLabel;
+        public ColoredBoxGroupAttribute(
+            string @group,
+            float  r           = 0,
+            float  g           = 0,
+            float  b           = 0,
+            float  a           = 0,
+            int    marginTop   = 0,
+            bool   showLabel   = true,
+            bool   centerLabel = false,
+            bool   boldLabel   = false,
+            float  order       = 0
+        ) : base(@group, showLabel, centerLabel, order)
+        {
+            R         = r;
+            G         = g;
+            B         = b;
+            A         = a;
+            MarginTop = marginTop;
+
+            BoldLabel = boldLabel;
+        }
+
+        public ColoredBoxGroupAttribute(
+            string @group,
+            string label,
+            float  r           = 0,
+            float  g           = 0,
+            float  b           = 0,
+            float  a           = 0,
+            int    marginTop   = 0,
+            bool   showLabel   = true,
+            bool   centerLabel = false,
+            bool   boldLabel   = false,
+            float  order       = 0
+        ) : base(@group, showLabel, centerLabel, order)
+        {
+            R         = r;
+            G         = g;
+            B         = b;
+            A         = a;
+            MarginTop = marginTop;
+            LabelText = label;
+            BoldLabel = boldLabel;
+        }
     }
 }
